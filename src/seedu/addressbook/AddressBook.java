1e1fad8c26198c75168c8dc7ce54908a857b330a
EDWIN-DELL\Edwin <edwingohhaoyu@gmail.com>
2017-08-31 03:33:07 +0800

    makePersonFromData change variable type from String[] to HashMap<String,String>

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -860,11 +860,11 @@ public class AddressBook {
      * @param email without data prefix
      * @return constructed person
      */
-    private static String[] makePersonFromData(String name, String phone, String email) {
-        final String[] person = new String[PERSON_DATA_COUNT];
-        person[PERSON_DATA_INDEX_NAME] = name;
-        person[PERSON_DATA_INDEX_PHONE] = phone;
-        person[PERSON_DATA_INDEX_EMAIL] = email;
+    private static HashMap<String,String> makePersonFromData(String name, String phone, String email) {
+        final HashMap<String,String> person = new HashMap<>();
+        person.put(PERSON_PROPERTY_NAME,name);
+        person.put(PERSON_PROPERTY_PHONE,phone);
+        person.put(PERSON_PROPERTY_EMAIL,email);
         return person;
     }
 
